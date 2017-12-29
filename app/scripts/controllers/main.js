4e4ed82fe24c52ac69b03f9b35d17f788d3d2bcc
swethanjali <swetha.madana91@gmail.com>
2017-12-29 17:51:06 -0600

    initial setup is done

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sierraapplicationpageApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sierraapplicationpageApp
+ */
+angular.module('sierraapplicationpageApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
